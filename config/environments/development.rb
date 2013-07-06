3aa2ae74f9145a1444428f52cebbdc1f7da4a592
Rachelle LeQuesne <rachellelequesne@Rachelle-MacBook-Pro.local>
2013-07-06 17:00:49 +1000

    add mail servers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,18 @@ RedLantern::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: ENV['HOST'] || 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['mail_server'] || "localhost",
+    :port => 1025,
+    :authentication => :login,
+    :user_name => ENV['mail_user'],
+    :password => ENV['mail_passwd']
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
