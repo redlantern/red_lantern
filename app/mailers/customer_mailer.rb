cffc71de96c2f0a4155ecfeffefb8a2f435d9189
Rachelle LeQuesne <Rachelle.LeQuesne@gmail.com>
2013-08-07 13:52:40 +1000

    extract only latest reply

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,9 +1,14 @@
 class CustomerMailer < ActionMailer::Base
   default from: SENDER_CUSTOMER
 
-  def new_reply_created reply_id
-    @reply = Reply.find reply_id
-    @ticket = @reply.ticket
+  def new_reply_created ticket_id
+
+  	@ticket = Ticket.find(ticket_id)
+    
+    @reply = @ticket.replies.last
+#    @reply = Reply.find reply_id
+#    @ticket = @reply.ticket
+
     mail(to: @ticket.sender, subject: "[#{@ticket.id}] #{@ticket.subject}")
 
   end
